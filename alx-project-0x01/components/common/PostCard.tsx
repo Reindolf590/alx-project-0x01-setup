77b4e8cdc79b5aaa55e7a8ccef504aef9ba32ca3
Reindolf590 <150713702+Reindolf590@users.noreply.github.com>
2025-06-01 18:24:25 +0000

    Update PostCard.tsx

--- alx-project-0x01/components/common/PostCard.tsx
+++ alx-project-0x01/components/common/PostCard.tsx
@@ -1,19 +1,17 @@
+import React from "react";
 import { PostProps } from "@/interfaces";
 
 const PostCard: React.FC<PostProps> = ({ title, body, userId, id }) => {
   return (
     <div className="max-w-xl mx-auto my-6 p-6 bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
-      <div className="mb-4">
-
-        <h2 className="text-2xl font-semibold text-gray-800">Welcome Page</h2>
-      </div>
-      <p className="text-gray-600">{body}</p>
-      <div className="mt-4 flex items-center justify-between text-sm text-gray-500">
-        <span>User ID : {userId}</span>
-        <span>User ID : {userId}</span>
+      <h2 className="text-xl font-bold mb-2 text-gray-800">{title}</h2>
+      <p className="text-gray-700 mb-4">{body}</p>
+      <div className="text-sm text-gray-500 flex justify-between">
+        <span>Post ID: {id}</span>
+        <span>User ID: {userId}</span>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default PostCard;
